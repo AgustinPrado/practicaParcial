98c0324bacb2f4c588cdbddedec9de808137d600
AgustinPrado <agusprado92@gmail.com>
2016-09-24 01:18:41 -0300

    20160924 - Parcial Fabrica Operario Array v1.0
    No está terminado, falta corregir un par de errores.

---
+++ parcial39_FabricaOperarioArray/Fabrica.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial39_FabricaOperarioArray
+{
+    public class Fabrica
+    {
+        private string _razonSocial;
+        private Operario[] _operarios;
+
+        public Fabrica()
+        {
+            this._operarios = new Operario[5];
+        }
+
+        public Fabrica(string razonSocial)
+            : this()
+        {
+            this._razonSocial = razonSocial;
+        }
+
+        public static bool operator == (Fabrica fbr, Operario op)
+        {
+            foreach (Operario item in fbr._operarios)
+            {
+                if(item != (object)null)
+                {
+                    if (item == op)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator != (Fabrica fbr, Operario op)
+        {
+            return !(fbr == op);
+        }
+
+        public static Fabrica operator + (Fabrica fbr, Operario op)
+        {
+            int indice = fbr.ObtenerIndice();
+            if ((indice != -1) && (fbr != op))
+            {
+                fbr._operarios[indice] = op;
+            }
+            else
+                Console.WriteLine("No hay mas cupo!!!");
+            return fbr;
+        }
+
+        public static Fabrica operator - (Fabrica fbr, Operario op)
+        {
+            int indice = fbr.ObtenerIndice(op);
+            if (indice != -1)
+            {
+                fbr._operarios[indice] = null;
+            }
+            else
+                Console.WriteLine("No se encontro al empleado!!!");
+            return fbr;
+        }
+
+        private int ObtenerIndice()
+        {
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                if (this._operarios[i] == (object)null)
+                    return i;
+            }
+            return -1;
+        }
+
+        private int ObtenerIndice(Operario op)
+        {
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                if (this._operarios[i] == op)
+                    return i;
+            }
+            return -1;
+        }
+
+        private float RetornarCostos()
+        {
+            float aux = 0;
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                if (this._operarios[i] != (object)null)
+                    aux += this._operarios[i].getSalario();
+            }
+            return aux;
+        }
+
+        private string MostrarOperarios()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                if (this._operarios[i] != (object)null)
+                    sb.AppendLine(Operario.Mostrar(this._operarios[i]));
+            }
+            return sb.ToString();
+        }
+
+        public string Mostrar()
+        {
+            return this.MostrarOperarios();
+        }
+
+        public static void MostrarCosto(Fabrica fbr)
+        {
+            Console.WriteLine(fbr.RetornarCostos());
+        }
+    }
+}
---
+++ parcial39_FabricaOperarioArray/Operario.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial39_FabricaOperarioArray
+{
+    public class Operario
+    {
+        private string _apellido;
+        private string _nombre;
+        private int _legajo;
+        private float _salario;
+
+        public Operario()
+        {
+            this._salario = 1500;
+        }
+
+        public Operario(string nombre, string apellido)
+            : this()
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;
+        }
+
+        public Operario(string nombre, string apellido, int legajo)
+            :this (nombre, apellido)
+        {
+            this._legajo = legajo;
+        }
+
+        public static bool operator == (Operario op1, Operario op2)
+        {
+            if(String.Compare(op1._apellido, op2._apellido) == 0)
+            {
+                if(String.Compare(op1._nombre, op2._nombre) == 0)
+                {
+                    if (op1._legajo == op2._legajo)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Operario op1, Operario op2)
+        {
+            return !(op1 == op2);
+        }
+
+        public float getSalario()
+        {
+            return this._salario;
+        }
+
+        public void setAumentarSalario(float aumento)
+        {
+            this._salario *= (1 + (aumento / 100));
+        }
+
+        public string ObtenerNombreYApellido()
+        {
+            StringBuilder sb = new StringBuilder(this._nombre + ", " + this._apellido);
+            return sb.ToString();
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.ObtenerNombreYApellido());
+            sb.AppendLine("Legajo: " + this._legajo);
+            sb.AppendLine("Salario: " + this.getSalario());
+            return sb.ToString();
+        }
+
+        public static string Mostrar(Operario op)
+        {
+            return op.Mostrar();
+        }
+    }
+}
